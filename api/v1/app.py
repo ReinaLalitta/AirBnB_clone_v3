cb6ed1d504685ec6c4025851b237c48647722005
LewisJassy <jassylewis70@gmail.com>
2024-05-01 10:59:57 +0300

    set up the api and get the status

---
+++ api/v1/app.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from flask import Flask, jsonify, make_response, request
+from models import storage
+from api.v1.views import app_views
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def close_storage(exception):
+    storage.close()
+    if __name__ == "__main__":
+        app.run(host='HBNB_API_HOST', port='HBNB_API_PORT', threaded=True)
